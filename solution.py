04e56f3ae2ffae6a8879e691c76fa020ac3e7744
vorvit <94832434+vorvit@users.noreply.github.com>
2023-04-03 02:37:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,18 +2,18 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
-from scipy.stats import t
+from scipy.stats import chi2
 
 
 chat_id = 1056349463 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    n = len(x)
+    sample_mean = np.mean(x)
+    sample_var = np.var(x, ddof=1)  # ddof=1 используется для расчета несмещенной выборочной дисперсии
     alpha = 1 - p
-    loc = x.mean()
-    t_value = t.ppf(1 - alpha/2, len(x)-1)
-    left = np.sqrt(len(x)*np.var(x)/t_value**2)
-    right = np.sqrt(len(x)*np.var(x)/t_value**2)
-    return (loc - left, loc + right)
+    chi2_left = chi2.ppf(alpha/2, n-1)
+    chi2_right = chi2.ppf(1-alpha/2, n-1)
+    left = np.sqrt((n-1)*sample_var/chi2_right)/0.31
+    right = np.sqrt((n-1)*sample_var/chi2_left)/0.31
+    return (sample_mean - left, sample_mean + right)
