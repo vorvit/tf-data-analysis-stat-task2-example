bd8a2fdc6e125f900781067401f3f43471a6b65a
vorvit <94832434+vorvit@users.noreply.github.com>
2023-04-06 20:38:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,11 +2,21 @@ import pandas as pd
 import numpy as np
 
 from scipy.stats import norm
-#from scipy.stats import chi2
+from scipy.stats import chi2
 
 
 chat_id = 1056349463 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
-    return (-(x.mean() * np.sqrt(len(x))) / (norm.ppf(alpha / 2) * np.sqrt(31)), -(x.mean() * np.sqrt(len(x))) / (norm.ppf(1 - alpha / 2) * np.sqrt(31)))
+    n = len(x)
+    
+    x2 = np.array([xi**2 for xi in x])
+    x2_mean = x2.mean()
+    
+    chi2_rv = chi2(df = 2 * n)
+    
+    left = chi2_rv.ppf(1 - alpha / 2)
+    right = chi2_rv.ppf(alpha / 2)
+    
+    return np.sqrt(n * x2_mean / (left * 31)), np.sqrt(n * x2_mean / (right * 31))
